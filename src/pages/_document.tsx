a4ac1b3110507570167ae12f286fb09a3ce6ebc7
Nazarul <nazarulxfitri@gmail.com>
2026-06-16 14:55:51 +0800

    integrate google adsense

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,10 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-1764114704213994"
+        />
         <link rel="icon" href={SITE_ICON} type="image/png" />
         <link rel="apple-touch-icon" href={SITE_ICON} />
       </Head>
